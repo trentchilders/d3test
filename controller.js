6afa424a336cd00d1475779bca24afa6feb447b1
Trent Childers <trent65@msn.com>
2018-05-12 11:38:49 -0500

    first commit

---
+++ controller.js
@@ -0,0 +1,7 @@
+var vertices = [{'x': 5, 'y': 5, color: 'red'},
+               {'x': 5, 'y': 100, color: 'green'},
+               {'x': 100, 'y': 100, color: 'blue'},
+               {'x': 100, 'y': 5, color: 'orange'}];
+
+
+var svg = d3.select('svg');
